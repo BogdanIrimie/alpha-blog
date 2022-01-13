a80dde5aac2106549d2669a67ee1586e46de309a
BogdanIrimie <bogdan.irimie@puppet.com>
2022-01-13 17:07:33 +0200

    Add delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,4 +35,11 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_url, notice: "Article was successfully destroyed."
+  end
 end
