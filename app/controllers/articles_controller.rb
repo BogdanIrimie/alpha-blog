082b8a4fcbf47ee51150f2271edd800776d5453c
BogdanIrimie <bogdan.irimie@puppet.com>
2022-01-13 15:09:23 +0200

    Add edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,37 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+    # render plain: @article.inspect
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article ws updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
